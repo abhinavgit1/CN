65e99a7b0ac6d0f05faf453326ac009e3284d349
abhinavgit1 <143893960+abhinavgit1@users.noreply.github.com>
2025-04-06 19:37:09 +0530

    Update Client.c

--- Multiserver/Client.c
+++ Multiserver/Client.c
@@ -1,53 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include <arpa/inet.h>
 #include <string.h>
-#include <unistd.h>
 
 #define PORT 7500
 
 void main() {
-	int serversocket, clientsocket;
-	struct sockaddr_in serveraddr, clientaddr;
+	int clientsocket;
+	struct sockaddr_in serveraddr;
 	socklen_t len;
-	char buffer[100], clientinfo[50];
-	pid_t child;
+	char buffer[100];
 
 	memset(&serveraddr, 0, sizeof(serveraddr));
-	memset(&clientaddr, 0, sizeof(clientaddr));
-	len = sizeof(clientaddr);
+	len = sizeof(serveraddr);
 
 	serveraddr.sin_family = AF_INET;
 	serveraddr.sin_port = PORT;
 	serveraddr.sin_addr.s_addr = INADDR_ANY;
 
-	serversocket = socket(AF_INET, SOCK_STREAM, 0);
-	bind(serversocket, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	clientsocket = socket(AF_INET, SOCK_STREAM, 0);
+	connect(clientsocket, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	printf("\nConnected to server.");
 
-	printf("\nWaiting for connections...");
-	listen(serversocket, 5);
+	printf("\nEnter the message: ");
+	while(fgets(buffer, sizeof(buffer), stdin) != "exit") {
+		send(clientsocket, buffer, sizeof(buffer), 0);
+		printf("\nMessage sent.");
 
-	while(1) {
-		clientsocket = accept(serversocket, (struct sockaddr*)&clientaddr, &len);
-		inet_ntop(AF_INET, &(clientaddr.sin_addr), clientinfo, sizeof(clientinfo));
-
-		if((child = fork()) == 0) {
-			close(serversocket);
-			printf("\nConnected to %s",clientinfo);
-			while(1) {
-				memset(buffer, 0, sizeof(buffer));
-				recv(clientsocket, buffer, sizeof(buffer), 0);
-
-				printf("\nClient: %s. Message: %s",clientinfo, buffer);
-				send(clientsocket, "Message recieved.", sizeof("Message recieved."), 0);
-				printf("\nReply sent to %s.",clientinfo);
-			}
-		}
-
-		close(clientsocket);
+		recv(clientsocket, buffer, sizeof(buffer), 0);
+		printf("\nReply: %s",buffer);
+		printf("\nEnter new message: ");
 	}
+
+	close(clientsocket);
 }
